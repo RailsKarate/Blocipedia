39b836c86e4a63bd385a3fbd6aa4769abf7f7612
Pedro <dropeholguin1994@gmail.com>
2015-09-19 09:51:00 -0500

    Created the test users Logging In and Singnin up

---
+++ spec/features/users/authetication_spec.rb
@@ -0,0 +1,24 @@
+require 'rails_helper'
+
+describe "Logging In" do
+
+	it "logs the user in and goes to the wikis" do
+		User.create(first_name: "Pedro", email: "pedro@gmail.com", password: "pedro12345", password_confirmation: "pedro12345")
+		visit new_user_session_path
+		fill_in "Email", with: "pedro@gmail.com"
+    	fill_in "Password", with: "pedro12345"
+    	click_button "Log in"
+
+    	expect(page).to have_content("Signed in successfully.")
+	end
+
+	it "diplays the email address in the event of a failed login" do
+	    visit new_user_session_path
+	    fill_in "Email", with: "pedro@gmail.com"
+	    fill_in "Password", with: "incorrect"
+	    click_button "Log in"
+
+	    expect(page).to have_content("")
+	    expect(page).to have_field("Email", with: "pedro@gmail.com")
+  	end
+end

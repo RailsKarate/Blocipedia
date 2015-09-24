e9f1468720cece21afd4162bba4900284f0699d3
Pedro <dropeholguin1994@gmail.com>
2015-09-24 14:55:03 -0500

    add the pundit policy

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,5 +4,14 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :authenticate_user! 
   include Pundit
+  rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
+
+  private
+
+  	def user_not_authorized
+  		flash[:alert] = "Access denied."
+  		redirect_to (request.referrer || wikis_path)
+  		
+  	end
 
 end

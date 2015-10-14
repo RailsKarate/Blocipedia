f3a18f839ae96f7894ca9f974e1e670ab2f8988f
Pedro <dropeholguin1994@gmail.com>
2015-10-14 15:00:39 -0500

    Add test to collaborators in spec

---
+++ spec/features/collaborators/user_collaborator_spec.rb
@@ -0,0 +1,103 @@
+require 'rails_helper'
+
+describe "Collaborators user" do
+
+	let(:other_user){ create(:user)}
+	let(:user) { wiki.user }
+	let!(:wiki) { create(:wiki, isprivate: true) }
+
+	def add_standard_collaborator 
+		login_as(other_user, scope: :user)
+		login_as(user, scope: :user)
+		user.update(role: 'premium')
+
+		visit "/wikis"
+
+		click_link wiki.title, wiki	
+
+		click_link "Edit"
+		expect(page).to have_content("Add/Delete Collaborator")
+
+		click_link "Add/Delete Collaborator"
+		
+		within "#user_2" do
+			click_button "Add Collaborator"
+		end
+
+		expect(page).to have_content(other_user.email)
+	end
+
+	def remove_standard_collaborator
+		add_standard_collaborator
+
+		click_link "Add/Delete Collaborator"
+
+		within "#user_2" do
+			click_button "Remove Collaborator"
+		end
+
+	end
+
+	it "you can't add the user multiple times" do
+		add_standard_collaborator
+
+		click_link "Add/Delete Collaborator"
+
+		within "#user_2" do
+			expect(page).to_not have_button "Add Collaborator"
+		end
+
+	end
+
+	it "you can see the private wiki in the wikis index" do
+		add_standard_collaborator
+
+		login_as(other_user, scope: :user)
+		visit "/wikis"
+
+		within "table" do
+			expect(page).to have_content(wiki.title)
+		end
+	end
+
+	it "you can edit the private wiki" do
+		add_standard_collaborator
+
+		login_as(other_user, scope: :user)
+		visit "/wikis"
+
+		click_link wiki.title, wiki	
+		click_link "Edit"
+						
+		wiki.update(title: "New title", 
+					body: "New body")
+
+		wiki.reload
+
+		expect(wiki.title).to eq("New title")
+		expect(wiki.body).to eq("New body")
+	end
+
+	it "remove a collaborator from a private wiki and you can't see the private wiki in the wikis index" do
+		remove_standard_collaborator
+
+		login_as(other_user, scope: :user)
+		visit "/wikis"
+
+		within "table" do
+			expect(page).to_not have_content(wiki.title)
+		end
+	end
+
+	it "you can't edit the private wiki" do
+		remove_standard_collaborator
+
+		login_as(other_user, scope: :user)
+		visit "/wikis"
+
+		expect(page).to_not have_link wiki.title
+		expect(page).to_not have_link "Edit"
+		
+	end
+
+end
--- spec/features/wikis/wiki_private_spec.rb
+++ spec/features/wikis/wiki_private_spec.rb
@@ -1,7 +1,8 @@
 require 'rails_helper'
 
 describe "wiki private" do
-	let(:user) { wiki.user}
+	let(:user) { wiki.user}	
+	let(:other_user){ create(:user)}
 
 	let!(:wiki) {create(:wiki, isprivate: true)}
 	let!(:other_wiki) {create(:wiki, title: "this wiki")}
@@ -9,12 +10,28 @@ describe "wiki private" do
 
 	it "you not see the private wiki in the wikis index for user role standard" do
 		login_as(user, scope: :user)
+		user.update(role: 'premium')
+
+		login_as(other_user, scope: :user)
 
 		visit "/wikis"
 		expect(Wiki.count).to eq(2)
 
 		within "table" do
 			expect(page).to_not have_content(wiki.title)
+			expect(page).to have_content(other_wiki.title)
+		end
+	end	
+
+	it "you see the private wiki and public in the wikis index for user role premium" do
+		login_as(user, scope: :user)
+		user.update(role: 'premium')
+
+		visit "/wikis"
+		expect(Wiki.count).to eq(2)
+
+		within "table" do
+			expect(page).to have_content(wiki.title)
 			expect(page).to have_content(other_wiki.title)
 		end
 	end	

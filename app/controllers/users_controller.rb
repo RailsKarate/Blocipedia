7fea4bbbd0af9ba819cbe1ca3b59b686f09a0ec6
Sebastian Velandia <sebas.velandia27@gmail.com>
2015-10-15 12:00:41 -0500

    Added user's profile page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,12 +1,22 @@
 class UsersController < ApplicationController
-	
-  def downgrade_account
-    current_user.update_attribute(:role, 'standard')
-    
-    current_user.wikis.each do |wiki|
-      wiki.update_attribute(:isprivate, false)
-    end
-    redirect_to wikis_path
-  end 
+
+	def show
+		@user = User.find(params[:id])
+		@wikis = @user.wikis.public_wikis(@user)
+		@private_wikis = Wiki.private_wikis(@user)
+		@collaborated_wikis = @user.collaborated_wikis
+  	end
+
+  	def update
+  	end
+		
+	def downgrade_account
+		current_user.update_attribute(:role, 'standard')
+
+		current_user.wikis.each do |wiki|
+		  wiki.update_attribute(:isprivate, false)
+		end
+		redirect_to wikis_path
+	end 
   
 end
